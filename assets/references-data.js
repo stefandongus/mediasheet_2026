231a9009a0423d1345bdd2310d03eab25ee4bf95
stefandongus <stefan@dongus.net>
2026-07-07 10:54:38 +0200

    Update references-data.js

--- assets/references-data.js
+++ assets/references-data.js
@@ -180,6 +180,57 @@ const FAVR_REFERENCES = [
     ]
   },
   {
+    name: "Atelier Valentin",
+    location: "Paris, France",
+    website: "http://www.ateliervalentin-paris.com/en/",
+    websiteLabel: "ateliervalentin-paris.com",
+    image: "../../assets/img/ref-valentin.png",
+    imageAlt: "Atelier Valentin Website Screenshot",
+    imageModule: "Try-On",
+    pages: [
+      {
+        label: "Showroom",
+        url: "https://www.favrspecs.com/fr/opticiens/a/paris/249/atelier-valentin/9913/pro/lunettes-de-vue/",
+        modules: ["Brand Portfolio", "Brand Pages", "Product Pages", "Virtual Try-On"],
+        type: "page"
+      }
+    ]
+  },
+  {
+    name: "Vandenbalck",
+    location: "Leuven, Belgium",
+    website: "https://vandenbalck.be/eyewear/",
+    websiteLabel: "vandenbalck.be",
+    image: "../../assets/img/ref-vandenbalck.png",
+    imageAlt: "Vandenbalck Website Screenshot",
+    imageModule: "Brand Post",
+    pages: [
+      {
+        label: "Pas nu online",
+        url: "https://www.favrspecs.com/en/opticians/in/leuven/586/vandenbalck-eyewear/10378/pro/eyeglasses/",
+        modules: ["Brand Portfolio", "Brand Pages", "Product Pages", "Virtual Try-On"],
+        type: "page"
+      }
+    ]
+  },
+  {
+    name: "City Optik Stockholm",
+    location: "Stockholm, Sweden",
+    website: "https://cityoptik.se/",
+    websiteLabel: "cityoptik.se",
+    image: "../../assets/img/ref-cityoptik.png",
+    imageAlt: "City Optik Stockholm Website Screenshot",
+    imageModule: "Brand Page",
+    pages: [
+      {
+        label: "Prova glasögon virtuellt",
+        url: "https://www.favrspecs.com/en/opticians/in/stockholm/338/city-optik/15975/pro/eyeglasses/",
+        modules: ["Brand Portfolio", "Brand Pages", "Product Pages", "Virtual Try-On"],
+        type: "page"
+      }
+    ]
+  },
+  {
     name: "Optik Schade",
     location: "Viersen, Deutschland",
     website: "https://optikschade.com/",
